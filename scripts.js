8689e2ddbab0731c97bc1e8ccecccbdaf91675da
Braden Mabey <braden.mabey@gmail.com>
2021-07-04 21:58:11 -0400

    added basic logic to app, technically completed the project

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
 const container = document.querySelector("div");
 
-for (let i = 0; i < 16; i++) {
+let size = prompt("Enter a number less than 100.")
+
+container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+for (let i = 0; i < (size ** 2); i++) {
 	let div = document.createElement("div");
 	div.classList.add("box");
 	container.appendChild(div);
 }
+
+function changeColor(e) {
+	console.log('herr');
+	e.target.style.backgroundColor = "black";
+}
+
+let boxes = Array.from(document.querySelectorAll(".box"));
+boxes.forEach(box => box.addEventListener('mouseenter', changeColor));
